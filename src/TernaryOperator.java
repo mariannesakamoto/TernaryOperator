fd3814e7df1cde443b8652bc68e0b2e206fc92d6
ALUNO <ALUNO@10.85.198.7>
2024-06-05 16:28:16 -0300

    Commit project TernaryOperator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,14 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int x = 10;
+        int result;
+
+        if (x > 5) {
+            result = x * 2;
+        } else {
+            result = x * 3;
+        }
+
+        System.out.println("Resultado: " + result);
+    }
+}
